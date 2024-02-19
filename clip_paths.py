8da051c513fc562710b4107838b7c286a01512a8
EthanRush <rushethan0@gmail.com>
2024-02-19 14:41:39 -0500

    Untrack files in .gitignore

--- clip_paths.py
+++
@@ -1,3 +0,0 @@
-outro_clip_path = "ENTER PATH TO OUTRO CLIP HERE"
-base_footage_path = "ENTER PATH TO BASE FOOTAGE HERE"
-output_path = "ENTER PATH TO OUTPUT CLIPS TO (or set equal to None to have it output to the base footage directory)"
