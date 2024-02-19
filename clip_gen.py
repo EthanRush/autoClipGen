dc224a29f22d279e84fcb66177a33cb9c3988671
EthanRush <rushethan0@gmail.com>
2024-02-19 13:58:45 -0500

    multiprocessing mode

--- clip_gen.py
+++ clip_gen.py
@@ -6,35 +6,14 @@ from moviepy.editor import *
 crossfade = 2
 
 
-def clip_gen(base_footage_path, clip_len, outro_clip_path, output_path=None):
-    
-    # Create object for base footage
+def clip_gen(base_footage_path, clip_count, outro_clip_path, base_length, base_filename, base_fps):
+    ind = 0
     base_footage = VideoFileClip(base_footage_path)
     # Create object for outro clip
     outro_clip = VideoFileClip(outro_clip_path)
-
-    #collect attributes for use in final
-    base_fps = base_footage.fps
-    base_filename = base_footage.filename.split(".")[0]
-    if(output_path is not None):
-        base_filename = base_filename.split("/")[-1]
-        base_filename = base_filename.split("\\")[-1]
-        base_filename = output_path + base_filename
-    #how long from base footage to cut for each clip
-    base_length = (clip_len - outro_clip.duration)
-
-    # how many clips can be greated evenly
-    clip_count = int(base_footage.duration / base_length)
-    
-    ind = 0
-    #loop through and create clips
-    base_footage.close()
-    outro_clip.close()
     for i in range(clip_count):
-        base_footage = VideoFileClip(base_footage_path)
         # Create object for outro clip
-        outro_clip = VideoFileClip(outro_clip_path)
-        outro_clip = outro_clip.resize((1080,1092))
+       
 
         start_time = i*base_length
         end_time = start_time + base_length
@@ -54,16 +33,10 @@ def clip_gen(base_footage_path, clip_len, outro_clip_path, output_path=None):
         final.write_videofile(base_filename + str(i) + ".mp4", base_fps)
         base_clip.close()
         final.close()
-        base_footage.close()
-        outro_clip.close()
+        
         ind += 1
-    print(ind)
     if(base_footage.duration % base_length != 0):
-        base_footage = VideoFileClip(base_footage_path)
         # Create object for outro clip
-        outro_clip = VideoFileClip(outro_clip_path)
-        outro_clip = outro_clip.resize((1080,1920))
-
         base_clip = base_footage.subclip((ind)*base_length)
         base_clip = base_clip.audio_fadeout(crossfade)
         base_clip = base_clip.resize((1080,1920))
@@ -79,6 +52,49 @@ def clip_gen(base_footage_path, clip_len, outro_clip_path, output_path=None):
         final.write_videofile(filename= base_filename + str(ind+1) + ".mp4", fps=base_fps)
         base_clip.close()
         final.close()
-        base_footage.close()
-        outro_clip.close()
+    return
     
+
+
+def clip_gen_parallel(base_footage_path, clip_index, outro_clip_path, base_length, base_filename, base_fps):
+    base_footage = VideoFileClip(base_footage_path)
+    # Create object for outro clip
+    start_time = clip_index*base_length
+    end_time = start_time + base_length
+    # getting subclip from base footage
+    base_clip = base_footage.subclip(start_time, end_time)
+    base_clip = base_clip.audio_fadeout(crossfade)
+    base_clip = base_clip.resize((1080,1920))
+
+    outro_clip = VideoFileClip(outro_clip_path)
+    # creating a composite video
+    final = concatenate_videoclips(clips=[base_clip.fx(transfx.crossfadeout,crossfade), outro_clip.set_start(base_clip.duration-crossfade).fx(transfx.crossfadein,crossfade)],
+                                    method="compose",
+                                    padding=-crossfade)
+    
+    
+    # showing final clip
+    final.write_videofile(base_filename + str(clip_index) + ".mp4", base_fps)
+    base_clip.close()
+    final.close()
+    return
+
+def last_clip(base_footage_path, clip_index, outro_clip_path, base_length, base_filename, base_fps):
+    base_footage = VideoFileClip(base_footage_path)
+    # Create object for outro clip
+    base_clip = base_footage.subclip((clip_index)*base_length)
+    base_clip = base_clip.audio_fadeout(crossfade)
+    base_clip = base_clip.resize((1080,1920))
+
+    outro_clip = VideoFileClip(outro_clip_path)
+
+    # creating a composite video
+    final = concatenate_videoclips(clips=[base_clip.fx(transfx.crossfadeout,crossfade), outro_clip.set_start(base_clip.duration-crossfade).fx(transfx.crossfadein,crossfade)],
+                                    method="compose",
+                                    padding=-crossfade)
+    
+    
+    # showing final clip
+    final.write_videofile(filename= base_filename + str(clip_index) + ".mp4", fps=base_fps)
+    base_clip.close()
+    final.close()
--- main.py
+++ main.py
@@ -1,6 +1,39 @@
-from clip_gen import clip_gen
+from clip_gen import clip_gen, clip_gen_parallel, last_clip
 from clip_paths import base_footage_path, outro_clip_path, output_path
-
+from moviepy.editor import *
+import multiprocessing as mp
 CLIP_LENGTH = 30
 if __name__ == '__main__':  
-    clip_gen(base_footage_path, CLIP_LENGTH, outro_clip_path, output_path)
+    
+    base_footage = VideoFileClip(base_footage_path)
+    # Create object for outro clip
+    outro_clip = VideoFileClip(outro_clip_path)
+
+    #collect attributes for use in final
+    base_fps = base_footage.fps
+    base_filename = base_footage.filename.split(".")[0]
+    if(output_path is not None):
+        base_filename = base_filename.split("/")[-1]
+        base_filename = base_filename.split("\\")[-1]
+        base_filename = output_path + base_filename
+    #how long from base footage to cut for each clip
+    base_length = (CLIP_LENGTH - outro_clip.duration)
+
+    # how many clips can be greated evenly
+    clip_count = int(base_footage.duration / base_length)
+    process_ct = min(max(mp.cpu_count() - 2, 1), clip_count)
+    ind = 0
+    #loop through and create clips
+    outro_clip = outro_clip.resize((1080,1092))
+    base_footage.close()
+    outro_clip.close()
+
+    if process_ct > 1:
+        pool = mp.Pool(process_ct) 
+        for x in range(0, clip_count):
+            res = pool.apply_async(clip_gen_parallel, args=(base_footage_path, x, outro_clip_path, base_length, base_filename, base_fps))
+        pool.close()
+        pool.join()
+        last_clip(base_footage_path, clip_count, outro_clip_path, base_length, base_filename, base_fps)
+    else:
+        clip_gen(base_footage_path, clip_count, outro_clip_path, base_length, base_filename, base_fps)
